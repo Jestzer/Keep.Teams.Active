e3bfb79363cee31a478304d34cb676e6be08b284
Jestzer <126624248+Jestzer@users.noreply.github.com>
2026-02-20 09:56:02 -0500

    fix: Reattempting to make timer actually 3 mintues. Allow multiple arguments to pull up version #. Updated version number.

--- Keep-Computer-Active.go
+++ Keep-Computer-Active.go
@@ -17,8 +17,8 @@ func main() {
 	// Print version number, if requested.
 	args := os.Args[1:]
 	for _, arg := range args {
-		if arg == "-version" {
-			fmt.Println("Version number: 1.3")
+		if arg == "-version" || arg == "--version" || arg == "-v" {
+			fmt.Println("Version number: 1.4")
 			os.Exit(0)
 		}
 	}
@@ -36,6 +36,9 @@ func main() {
 		os.Exit(0)
 	}()
 
+	ticker := time.NewTicker(3 * time.Minute)
+	defer ticker.Stop()
+
 	for {
 		// Linux gets angrryyyy when you try to use F13.
 		if runtime.GOOS == "linux" {
@@ -45,13 +48,9 @@ func main() {
 			robotgo.KeyTap("f13")
 		}
 
-		fmt.Println("Continuing to keep computer active. See you in roughly 3 minutes! The current time is", time.Now().Local().Format("03:04:05 PM."))
+		fmt.Println("Continuing to keep computer active. See you in 3 minutes! The current time is", time.Now().Local().Format("03:04:05 PM."))
 
-		// Wait for 3 minutes.
-		// It will seem like the time is slowly drifting. That's because this program and the for loop don't run instantly...
-		// ... and the 3 minute timer doesn't take this into account. I could make it /seemingly/ run without any drifting...
-		// ... but there's really no point in adding all that code for something that really doesn't make any difference.
-		time.Sleep(3 * time.Minute)
+		<-ticker.C
 	}
 
 }
